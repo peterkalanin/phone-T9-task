b3aec327ceab1a88a5b2e0fcae37dffe418ae9ae
kalan <kalanin.peto@gmail.com>
2022-01-16 11:10:25 +0100

    add: phone button component

---
+++ client/src/components/PhoneButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PhoneButton() {
+  return (
+    <div className='phone-keybutton'>
+      <div className='phone-keybutton-number'>1</div>
+      <div className='phone-keybutton-letters'>abc</div>
+    </div>
+  );
+}
