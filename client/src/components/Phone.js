1b3a8ccdc2dec068698586da6d0527b33a688134
kalan <kalanin.peto@gmail.com>
2022-01-16 11:15:02 +0100

    feat: button layout

--- client/src/components/Phone.js
+++ client/src/components/Phone.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import PhoneButton from './PhoneButton';
 
 export default function Phone() {
   const [numberKeys, setNumberKeys] = useState([]);
 
   useEffect(() => {}, [numberKeys]);
+
+  const buttons = [
+    { key: '1', letters: [''] },
+    { key: '2', letters: ['a', 'b', 'c'] },
+    { key: '3', letters: ['d', 'e', 'f'] },
+    { key: '4', letters: ['g', 'h', 'i'] },
+    { key: '5', letters: ['j', 'k', 'l'] },
+    { key: '6', letters: ['m', 'n', 'o'] },
+    { key: '7', letters: ['p', 'q', 'r', 's'] },
+    { key: '8', letters: ['t', 'u', 'v'] },
+    { key: '9', letters: ['w', 'x', 'y', 'z'] },
+    { key: '*', letters: ['+'] },
+    { key: '0', letters: ['⎵'] },
+    { key: '#', letters: ['⇧'] },
+  ];
 
   return (
     <div className='phone-wrapper'>
@@ -16,7 +32,11 @@ export default function Phone() {
           <div className='phone-speaker-dot'></div>
         </div>
         <div className='phone-screen-wrapper'></div>
-        <div className='phone-keyboard'></div>
+        <div className='phone-keyboard'>
+          {buttons.map((button, index) => (
+            <PhoneButton button={button} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- client/src/components/PhoneButton.js
+++ client/src/components/PhoneButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function PhoneButton() {
+export default function PhoneButton(props) {
   return (
     <div className='phone-keybutton'>
-      <div className='phone-keybutton-number'>1</div>
-      <div className='phone-keybutton-letters'>abc</div>
+      <div className='phone-keybutton-number'>{props.button.key}</div>
+      <div className='phone-keybutton-letters'>
+        {props.button.letters.join('')}
+      </div>
     </div>
   );
 }
