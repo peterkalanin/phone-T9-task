1de60c7114511947e8cfc72ea83151a0e46a3ebe
Peter Kalanin <kalanin.peto@gmail.com>
2022-01-17 09:16:39 +0100

    refact: renamed WordsController

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {
-  getWords
-} = require('./controllers/words')
+const WordsController = require('./controllers/words.controller');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post('/words', getWords);
+app.post('/words', WordsController.getWords);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
