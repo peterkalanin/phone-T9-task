88bf55f5a491b9c9f05b02ad1b787b977c7f4b05
Peter Kalanin <kalanin.peto@gmail.com>
2022-01-17 17:36:05 +0100

    add: cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const WordsController = require('./controllers/words.controller');
 
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
+app.use(cors())
 
 app.post('/words', WordsController.getWords);
 
