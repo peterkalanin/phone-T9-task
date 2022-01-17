e2e7babb8fe2864f3717672f96b7e1d755392f1e
Peter Kalanin <kalanin.peto@gmail.com>
2022-01-17 20:05:51 +0100

    fix: phone lines key

--- client/src/components/PhoneScreen.js
+++ client/src/components/PhoneScreen.js
@@ -11,7 +11,13 @@ export default function PhoneScreen(props) {
     <div className="phone-screen">
       <div className="phone-text">{text}</div>
       <div className="phone-lines">
-        {words.map(word => {return <div className="phone-line">{word}</div>})}
+        {words.map((word, index) => {
+          return (
+            <div className="phone-line" key={index}>
+              {word}
+            </div>
+          );
+        })}
       </div>
       <div className="phone-actions">
         <div className="phone-action">{leftActionLabel}</div>
