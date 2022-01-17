4440b7b0992130eecb4a5b7d660ac25809059596
Peter Kalanin <kalanin.peto@gmail.com>
2022-01-17 09:11:32 +0100

    feat: WordsController implementation

--- server/controllers/words.js
+++ server/controllers/words.js
@@ -1,10 +1,14 @@
+const WordsService = require("../services/words.service");
+
 const getWords = (req, resp) => {
     const body = req.body;
     let reqKeys = body['keys[]'];
 
-    console.log(body);
+    console.log(reqKeys);
 
-    res.json([]);
+    let words = WordsService.getLetterCombinations(reqKeys);
+
+    res.json(words);
 }
 
 module.exports = {
